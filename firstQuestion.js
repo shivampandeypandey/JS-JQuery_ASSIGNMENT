fbda41036dd0daf37f3923af96d114f5f77ca8e3
Shivam Pandey <49595915+shivampandeypandey@users.noreply.github.com>
2021-08-25 21:53:13 +0530

    Update firstQuestion.js

--- firstQuestion.js
+++ firstQuestion.js
@@ -1,16 +1,15 @@
-let PARA = `Thank you so much for contacting us. Dreamland guest relations. You're speaking with Ronaldo. 
-May I have your name, please? Yes, sure. My name is robin soleimani. 
+let PARA = `Thank you so much for contacting us. Dreamland guest relations. 
+You're speaking with Ronaldo. May I have your name, please? Yes, sure. My name is robin soleimani.
 What is your phone number? It is 9876535362 as mentioned in the book Diary. 
 Thank you so much for providing the info!`;
-let SENT = PARA.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")
-console.log(SENT);
+let SENTENCE = PARA.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")
 
 let res = 1;
 
-for (let S of SENT){
-    if (S.split(' ').length >= 4) {
-        let maskedSent = `${res}. ${S.replace(/[6-9]\d{9}/g, 'XXXXXXXXXX').trim()}`;
-        console.log(maskedSent);
+for (let Sent of SENTENCE){
+    if (Sent.split(' ').length >= 4){ 
+        let masked = `${res}. ${Sent.replace(/[6-9]\d{9}/g, 'XXXXXXXXXX').trim()}`;
+        console.log(masked);
         res++;
     }
 }
